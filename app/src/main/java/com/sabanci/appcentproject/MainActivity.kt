74d442088f3865ab85381d0293dc88b35a73f71b
Denizhan Altan <denizhanaltan34@gmail.com>
2024-05-14 03:12:58 +0300

    Fragments added

--- app/src/main/java/com/sabanci/appcentproject/MainActivity.kt
+++ app/src/main/java/com/sabanci/appcentproject/MainActivity.kt
@@ -2,10 +2,42 @@ package com.sabanci.appcentproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.sabanci.appcentproject.databinding.ActivityMainBinding
+import com.sabanci.appcentproject.fragment.FavoritesFragment
+import com.sabanci.appcentproject.fragment.NewsFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(NewsFragment())
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener{
+
+            when(it.itemId){
+                R.id.news_icon -> replaceFragment(NewsFragment())
+                R.id.favorites_icon -> replaceFragment(FavoritesFragment())
+
+                else ->{
+
+                }
+            }
+
+            true
+        }
+    }
+
+
+    private fun replaceFragment(fragment : Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
